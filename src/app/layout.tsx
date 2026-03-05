ad2fe4e4c2651a4ca51a2a029b00681c12bc9f3e
Nick Granados <53047332+internick2017@users.noreply.github.com>
2026-03-05 17:35:06 -0300

    Add Google Ads script to layout and update index file for new layout chunk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from '@/components/theme-provider'
+import Script from 'next/script'
 import './globals.css'
 
 export default function RootLayout({
@@ -8,6 +9,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6256775509106577"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
       <body>
         <ThemeProvider
           attribute="class"
